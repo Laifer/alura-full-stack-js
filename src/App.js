a2244bc25d428fa5ae488bc046afefeab6a20832
Laifer <gabriel.alves@cotefacil.com>
2023-04-28 17:31:19 -0300

    feat: finishing eighth step of second class
    Adding some header options, using list and mapping with JSX to create some main options and some icons, adding text and images with that tecnique; adding some styling to the components too and made the logo keep the format with the rest of the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './styles/App.css';
 import Logo from './components/Logo'
+import profile from './static/perfil.svg'
+import bag from './static/sacola.svg'
+
+const textOptions = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+const icons = [profile, bag]
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo/>
+        <ul className='header-options'>
+          { textOptions.map( (text) => (
+              <li className='option'><p>{text}</p></li>
+            )
+          )}
+        </ul>
+        <ul className='icons'>
+          { icons.map ( (icon) => (
+              <li className='icon'><img src={icon}/></li>
+            )
+          )}
+        </ul>
       </header>
     </div>
   );
