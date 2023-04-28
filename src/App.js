d9228b10bd288b2f120e01f863296379714643f7
Laifer <gabriel.alves@cotefacil.com>
2023-04-28 16:03:06 -0300

    refactor: removed test to keep going with the alura classes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import './styles/App.css';
 function App() {
   return (
     <div>
-      <pre>
-        <code>some text to test code html<br/>
-          foo bar():<br/>
-            foo = "bar"
-        </code>
-      </pre>
       <header>
 
       </header>
