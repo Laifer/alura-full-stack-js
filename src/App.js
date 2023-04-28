9334ebe47a94c191291dac67df13b299306408f5
Laifer <gabriel.alves@cotefacil.com>
2023-04-28 16:29:20 -0300

    feat: componentization of the log, as a react component and separated the stylesheet from main stylesheet, ending fourth alura class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './styles/App.css';
-import logo from './static/logo.svg'
+import Logo from './components/Logo'
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <div className='logo'>
-          <img src={logo} alt='A traingular logo that takes you to homepage when clicked'/>
-          <p><strong>Alura</strong> Books</p>
-        </div>
+        <Logo/>
       </header>
     </div>
   );
---
+++ src/components/Logo/index.js
@@ -0,0 +1,13 @@
+import './style.css'
+import logo from '../../static/logo.svg'
+
+function Logo() {
+    return (
+        <div className='logo'>
+          <img src={logo} alt='A traingular logo that takes you to homepage when clicked'/>
+          <p><strong>Alura</strong> Books</p>
+        </div>
+    )
+}
+
+export default Logo
