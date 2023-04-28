f82e98037c5be31e891e9c9f672cc234cfa5e4be
Laifer <gabriel.alves@cotefacil.com>
2023-04-28 16:16:02 -0300

    feat: add logo and text logo on screen and add starting styling to make it appear inline

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './styles/App.css';
+import logo from './static/logo.svg'
 
 function App() {
   return (
-    <div>
-      <header>
-
+    <div className='App'>
+      <header className='App-header'>
+        <div className='logo'>
+          <img src={logo} alt='A traingular logo that takes you to homepage when clicked'/>
+          <p><strong>Alura</strong> Books</p>
+        </div>
       </header>
     </div>
   );
