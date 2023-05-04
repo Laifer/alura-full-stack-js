327e8167ff94938320e44ca598b4cd550943add3
Laifer <gabriel.alves@cotefacil.com>
2023-05-04 15:48:25 -0300

    feat: add one prop to change the background color, as I'm working with different coloring in the page, as compared to the example from the classes

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Title = styled.h2`
     width: 100%;
     padding: 30px 0;
-    background-color: antiquewhite;
+    background-color: ${ props => props.bgColor || 'antiquewhite'};
     color: ${ props => props.titleColor || '#000'};
     font-size: ${ props => props.fontSize || '18px'};
     text-align: ${ props => props.textAlign || 'center'};
