c506682514e6e9827410deada462e01c3516cbb3
Laifer <gabriel.alves@cotefacil.com>
2023-05-05 18:13:49 -0300

    refactor: removed unused import

--- src/routes/Favorites.js
+++ src/routes/Favorites.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 
 import Search from '../components/Search';
-import RecentReleases from '../components/RecentReleases';
 
 const AppContainer = styled.div`
   width: 100vw;
