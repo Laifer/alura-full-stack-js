123241ce7d35a2073ad12be610748dec2ee36eff
Laifer <gabriel.alves@cotefacil.com>
2023-05-08 18:47:57 -0300

    feat: end of activity 3 class 4
    In that activity the teacher left us the styling he used and I just used it and adjusted the margin so it didn't bug with the header

--- src/routes/Favorites.js
+++ src/routes/Favorites.js
@@ -2,11 +2,47 @@ import styled from 'styled-components'
 import { useEffect, useState } from 'react'
 
 import { getFavorites } from '../services/favorites'
+import bookImg from '../static/livro.png'
 
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg, #002F52, #326589);
+`
+
+const ResultContainer = styled.div`
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: center;
+`
+
+const Result = styled.div`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   margin: 20px 0;
+   cursor: pointer;
+   text-align: center;
+   padding: 0 100px;
+   p {
+       width: 200px;
+       color: #FFF;
+   }
+   img {
+       width: 100px;
+   }
+   &:hover {
+       border: 1px solid white;
+   }
+`
+
+const Title = styled.h2`
+   color: #FFF;
+   font-size: 36px;
+   text-align: center;
+   width: 100%;
+   padding-top: 35px;
+   margin: 0;
 `
 
 function Favorites() {
@@ -24,9 +60,15 @@ function Favorites() {
 
   return (
     <AppContainer>
-      {favorites.map( favorite => (
-        <p>{favorite.name}</p>
-      ) )}
+      <Title>Aqui estão seus livros favoritos:</Title>
+      <ResultContainer>
+      {favorites.length !== 0 ? favorites.map( favorite => (
+        <Result>
+          <p>{favorite.name}</p>
+          <img src={bookImg}/>
+        </Result>
+      ) ) : null}
+      </ResultContainer>
     </AppContainer>
   );
 }
